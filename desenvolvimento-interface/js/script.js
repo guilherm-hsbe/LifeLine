5999cc987d4a5475800b1d918ebe5c497b130b73
guilherm-hsbe <guilherm3hsb@gmail.com>
2023-10-16 21:38:18 -0300

    tasklist [arqs. na desc]: implementação frontend
    arquivos [index.html; contact.html; style1.css; script.js]
    
    [fix]
    - ajustar funcionamento do footer: fixado no fim da página
    
    [feat]
    - complementa o form com novos campos e funcinalidades de seleção de opções de assunto e tipo de pessoa
    - inclui função JS de formatar número do telefone
    - otimiza comentários, separadores e estrutura de páginas

--- desenvolvimento-interface/js/script.js
+++ desenvolvimento-interface/js/script.js
@@ -1,4 +1,8 @@
-// Ativar barra de pesquisa com atalho
+//? ─── Navbar ──────────────────────────────────────────────────────────────────
+
+/* ------------------------------------------------------------
+ * Ativar barra de pesquisa com atalho
+ */
 const searchBar = document.getElementById("searchBar");
 
 function activateSearchBar() {
@@ -12,9 +16,88 @@ document.addEventListener("keydown", function (event) {
     }
 });
 
-// Mudar display de comando da search bar dependendo do tipo de dispositivo do usuário (Mac ou Windows)
+/* ------------------------------------------------------------
+ * Mudar display de comando da search bar dependendo do tipo de dispositivo do usuário (Mac ou Windows)
+ */
 const searchBarLabel = document.getElementById("searchBarLabel");
 const isMacOS = navigator.userAgent.toUpperCase().indexOf('MAC') >= 0;
 
 if (isMacOS) { searchBarLabel.innerText = "CMD + K"; }
 else { searchBarLabel.innerText = "CTRL + K"; }
+
+//? ─── Contato ──────────────────────────────────────────────────────────────────
+
+/* ------------------------------------------------------------
+ * Formatar número de telefone
+ */
+document.getElementById("tbTel").addEventListener("input", function (e) {
+    const input = e.target;
+    const value = input.value.replace(/\D/g, "");
+    
+    if (value.length >= 1) {
+        input.value = `(${value.substring(0, 2)}) ${value.substring(2, 6)}-${value.substring(6, 11)}`;
+    } else input.value = value;
+});
+
+/* ------------------------------------------------------------
+ * Apresentação dinâmica das subopções do seletor de assunto
+ */
+const tipoAssunto = document.getElementById('dlTipoAssunto');
+const tipoAssuntoSM = document.getElementById('dlTipoAssuntoSM');
+const tipoAssuntoSF = document.getElementById('dlTipoAssuntoSF');
+const tipoAssuntoDR = document.getElementById('dlTipoAssuntoDR');
+const tipoAssuntoGR = document.getElementById('dlTipoAssuntoGR');
+const tipoAssuntoOutro = document.getElementById('tbTipoAssuntoOutro');
+
+function hideAllSelects() {
+    tipoAssuntoSM.style.display = 'none';
+    tipoAssuntoSF.style.display = 'none';
+    tipoAssuntoDR.style.display = 'none';
+    tipoAssuntoGR.style.display = 'none';
+    tipoAssuntoOutro.style.display = 'none';
+}
+
+hideAllSelects();
+tipoAssunto.style.display = 'flex';
+
+tipoAssunto.addEventListener('change', function () {
+    hideAllSelects();
+    const valorSelecTipoAssunto = tipoAssunto.value;
+
+    if (valorSelecTipoAssunto === 'op1') tipoAssuntoSM.style.display = 'flex';
+    else if (valorSelecTipoAssunto === 'op2') tipoAssuntoSF.style.display = 'flex';
+    else if (valorSelecTipoAssunto === 'op3') tipoAssuntoDR.style.display = 'flex';
+    else if (valorSelecTipoAssunto === 'op4') tipoAssuntoGR.style.display = 'flex';
+    else if (valorSelecTipoAssunto === 'op5') tipoAssuntoOutro.style.display = 'flex';
+});
+tipoAssunto.dispatchEvent(new Event('change'));
+
+/* ------------------------------------------------------------
+ * Especificação de pessoa por opção "Outro"
+ */
+const tipoPessoa = document.getElementById('dlTipoPessoa');
+const tipoPessoaOutro = document.getElementById('tbTipoPessoaOutro');
+
+tipoPessoaOutro.style.display = 'none';
+tipoPessoa.style.display = 'flex';
+
+tipoPessoa.addEventListener('change', function() {
+    tipoPessoaOutro.style.display = 'none';
+    const valorSelecTipoPessoa = tipoPessoa.value;
+    if (valorSelecTipoPessoa === 'op8') tipoPessoaOutro.style.display = 'flex';
+});
+tipoPessoa.dispatchEvent(new Event('change'));
+
+/* ------------------------------------------------------------
+ * Checar tamanho do arquivo do anexo
+ */
+function checkFileSize(input) {
+    const maxFileSize = 10 * 1024 * 1024;
+    const fileSize = input.files[0].size;
+    const errorElement = document.getElementById('file-size-error');
+
+    if (fileSize > maxFileSize) {
+        errorElement.textContent = 'O arquivo excede o tamanho máximo permitido (10 MB).';
+        input.value = '';
+    } else errorElement.textContent = '';
+}
